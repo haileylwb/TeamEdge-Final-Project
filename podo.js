0a4b52f41452cf948c012947557b968b94bc4873
Hailey WB <73897167+hileelwb@users.noreply.github.com>
2021-05-22 08:32:27 -0700

    Update podo.js

--- podo.js
+++ podo.js
@@ -1,55 +1,36 @@
-console.log("hi");
+console.log("this program works!");
 
 var pomodoroVar = 5 //25 min later
 var shortBreakVar = 6//1000*60*5
 var longBreakVar = 8//1000*60*10
 
-// var minutes;
-// var seconds;
-// var formatted = minutes.toString() + ':' + seconds.toString();
-
+// pomodoro functions
 var interval;
+var timeVar;
+
 function pomodoro(){
-    interval = setInterval(function(){
-        pomodoroVar -= 1;
-        // minutes =  Math.floor(pomodoroVar / 60);
-        // seconds = pomodoroVar % 60
-        // new Date(pomodoroVar * 1000).toISOString().substr(11,8)
-        console.log(pomodoroVar);
-        if (pomodoroVar == 0) {
-            clearInterval(interval)
-        }
-    }, 1000)
+    console.log("Commencing POMODORO");
+    return timeVar = pomodoroVar;
 }
 
 function shortBreak(){
-    interval = setInterval(function(){
-        shortBreakVar -= 1;
-        // minutes =  Math.floor(pomodoroVar / 60);
-        // seconds = pomodoroVar % 60
-        // new Date(pomodoroVar * 1000).toISOString().substr(11,8)
-        console.log(shortBreakVar);
-        if (shortBreakVar == 0) {
-            clearInterval(interval)
-        }
-    }, 1000)
+    console.log("Commencing SHORT BREAK");
+    return timeVar = shortBreakVar;
 }
 
 function longBreak(){
+    console.log("Commencing LONG BREAK");
+    return timeVar = longBreakVar;
+}
+
+
+// starts the timer
+function start(){
     interval = setInterval(function(){
-        longBreakVar -= 1;
-        // minutes =  Math.floor(pomodoroVar / 60);
-        // seconds = pomodoroVar % 60
-        // new Date(pomodoroVar * 1000).toISOString().substr(11,8)
-        console.log(longBreakVar);
-        if (longBreakVar == 0) {
+        timeVar -= 1;
+        console.log(timeVar);
+        if (timeVar == 0) {
             clearInterval(interval)
         }
     }, 1000)
 }
-
-// pomodoro();
-
-// shortBreak();
-
-longBreak();
