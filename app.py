0f8e9b2045fcc1e4761d7c4492196de2aa9ed440
Hailey WB <73897167+hileelwb@users.noreply.github.com>
2021-06-05 09:08:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('podo.html')
+    storing = requests.get('https://zenquotes.io/api/random')
+    json_storing = storing.json()
+    quote = json_storing[0]
+    return render_template('podo.html', quote=quote)
 
 
 if __name__ == '__main__':
